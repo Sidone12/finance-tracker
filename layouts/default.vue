ea4ec0d891f2add2c7cf2233dc342816fc944f33
Oleksii <sidone666@gmail.com>
2024-09-08 10:58:38 +0300

    create default layout styles + header

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+     <AppHeader />
+    <main class="my-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+// Link to Google Fonts
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
+
+<style>
+/* Set the font family */
+body {
+  font-family: 'Inter';
+}
+/* Set the background color for */
+body {
+  @apply dark:bg-gray-900 bg-white
+}
+</style>
