df371a1e7762d42b8c5c2cd5a50f00f89143726c
Oleksii <o.katerynchuk@viseven.com>
2024-09-24 16:13:07 +0300

    add transaction modal component + func

---
+++ components/transactions-modal.vue
@@ -0,0 +1,30 @@
+<template>
+    <UModal v-model="isOpen">
+        <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+            <template #header>
+                Add Transaction
+            </template>
+
+            <div>Hello</div>
+
+            <template #footer>
+                some footer text
+            </template>
+        </UCard>
+    </UModal>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    modelValue: Boolean
+})
+const emit = defineEmits(['update:modelValue'])
+
+const isOpen = computed({
+    get: () => props.modelValue,
+    set: (value)=> emit('update:modelValue',value)
+})
+
+</script>
+
+<style></style>
