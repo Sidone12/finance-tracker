d82c9f9379f0b8e280872a203bc8cb28ee31335d
Oleksii <sidone666@gmail.com>
2024-11-01 16:41:21 +0200

    add pages for settings

---
+++ pages/settings/profile.vue
@@ -0,0 +1,57 @@
+<template>
+<UForm :state="state" :schema="schema" @submit.prevent="saveProfile">
+    <UFormGroup class="mb-4" label="Full Name" name="name">
+        <UInput v-model:="state.name" class="" />
+    </UFormGroup>
+    <UFormGroup class="mb-4" label="Email" name="email" help="You will resive confirmation email on bouth the old and new addresses if you modify the email address.">
+        <UInput v-model:="state.email" class="" />
+    </UFormGroup>
+    <UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
+</UForm>
+</template>
+
+<script setup>
+import { z } from 'zod'
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const { toastSucces, toastError } = useAppToast()
+const pending = ref(false)
+
+const state = ref({
+    name: user.value.user_metadata?.full_name,
+    email: user.value.email
+})
+
+const schema = z.object({
+    name: z.string().min(2).optional(),
+    email: z.string().email()
+})
+
+const saveProfile = async () => {
+    pending.value = true
+    try {
+        const data = {
+            full_name: state.value.name
+        }
+        if (state.value.email != user.value.email) {
+            data.email = state.value.email
+        }
+
+
+        const { error } = await supabase.auth.updateUser(data)
+        if (error) throw error
+        toastSucces({
+            title: 'Profile updated',
+            describtion: 'Your profile has been updated.'
+        })
+    } catch (error) {
+        toastError({
+            title: 'Error updating profile.',
+            describtion: error.message
+        })
+    } finally {
+        pending.value = false
+    }
+}
+</script>
